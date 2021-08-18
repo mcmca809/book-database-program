65eca5ba6bb5a38dd9bdaa434f119e808bbceb0d
Cameron McMillan <mcmillan.cameron@gmail.com>
2021-08-18 19:17:16 +1000

    added our database model

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-# import models
+from models import (Base, session, 
+                    Book, engine)
 # main menu - add, search, analysis, exit, view 
 # add books to the database
 # edit books
@@ -6,3 +7,7 @@
 # search books
 # data cleaning
 # loop runs program
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
